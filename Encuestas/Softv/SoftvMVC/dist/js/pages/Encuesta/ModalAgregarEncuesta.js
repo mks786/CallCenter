34f80a593171026bab40a45848afae533d54a798
ifonseca <lmagers@gmail.com>
2016-05-17 10:04:19 -0500

    valdiacion de nombre y descripcion de encuesta

--- Encuestas/Softv/SoftvMVC/dist/js/pages/Encuesta/ModalAgregarEncuesta.js
+++ Encuestas/Softv/SoftvMVC/dist/js/pages/Encuesta/ModalAgregarEncuesta.js
@@ -3,32 +3,40 @@
 
 $('#guardarEncuesta').click(function(){
     //validamos que existan preguntas en la tabla y los detalles de la encuesta no esten vacios 
-    console.log(Lista_preguntas);
-    console.log(encuesta);
-    if ($('#TbodyPreguntas').children().length == 0) {
-        swal("A ocurrido un error", "Tu encuesta no contiene preguntas", "error");
+    var titulo_encuesta = $('#nombreEncuesta').val();
+    var descripcion_encuesta = $('#Descripcion_encuesta').val();
+    
+    if (titulo_encuesta == "") {
+        swal("A ocurrido un error", "El titulo de la encuesta es obligatorio", "error");
+    } else if (descripcion_encuesta == "") {
+        swal("A ocurrido un error", "La descripción de la encuesta es obligatorio", "error");
     } else {
-        var encuesta = {
-            IdEncuesta: 4,
-            TituloEncuesta: $('#nombreEncuesta').val(),
-            Descripcion: $('#Descripcion_encuesta').val(),
-           
-        }
-        var usuario= document.getElementById("username").innerHTML;
-
-        $.ajax({
-            url: "Encuesta/Create/",
-            type: "POST",
-            data: { 'encuesta': encuesta, 'Preguntas': Lista_preguntas, 'respuestas': Lista_opciones, 'usuario': usuario },
-            success: function (data, textStatus, jqXHR) {
-                //data - response from server
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
+        if ($('#TbodyPreguntas').children().length == 0) {
+            swal("A ocurrido un error", "Tu encuesta no contiene preguntas", "error");
+        } else {
+            var encuesta = {
+                IdEncuesta: 4,
+                TituloEncuesta: titulo_encuesta,
+                Descripcion: descripcion_encuesta,
 
             }
-        });
+            var usuario = document.getElementById("username").innerHTML;
 
+            $.ajax({
+                url: "Encuesta/Create/",
+                type: "POST",
+                data: { 'encuesta': encuesta, 'Preguntas': Lista_preguntas, 'respuestas': Lista_opciones, 'usuario': usuario },
+                success: function (data, textStatus, jqXHR) {
+                    //data - response from server
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+
+                }
+            });
+
+        }
     }
+   
    
    
 
