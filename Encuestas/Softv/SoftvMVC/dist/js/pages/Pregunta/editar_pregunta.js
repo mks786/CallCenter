975faf12f5058a04e1112f268434dbd8c7d11d5c
isfon <lamgers@gmail.com>
2016-05-25 11:01:41 -0500

    correcion de validaciones

---
+++ Encuestas/Softv/SoftvMVC/dist/js/pages/Pregunta/editar_pregunta.js
@@ -0,0 +1,12 @@
+﻿$('#guardar_pregunta').click(function () {
+    var nombre_pregunta = $('#pregunta_nombre').val();
+    var tipo_pregunta = $("#id_tipo_pregunta option:selected").val();
+    if (tipo_pregunta == 1) {
+        var detallePregunta = {};
+        detallePregunta.IdPregunta = generateUUID();
+        detallePregunta.Pregunta = nombre_pregunta;
+        detallePregunta.IdTipoPregunta = tipo_pregunta;
+        preguntas_lista.push(detallePregunta);
+    }
+    console.log(preguntas_lista);
+});
